9a32483e92290fdcee9dd8dc00066ad0574b2616
Nick Huang <nick@langchain.dev>
2024-11-18 14:27:56 -0500

    Update notebooks

--- notebooks/module_2/utils.py
+++ notebooks/module_2/utils.py
@@ -3,13 +3,22 @@ import tempfile
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders.sitemap import SitemapLoader
 from langchain_community.vectorstores import SKLearnVectorStore
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+import nest_asyncio
+import operator
+from langchain.schema import Document
+from langchain_core.messages import HumanMessage, AnyMessage, get_buffer_string
+from langgraph.graph import StateGraph, START, END
+from IPython.display import Image, display
+from typing import List
+from typing_extensions import TypedDict, Annotated
 
 RAG_PROMPT = """You are an assistant for question-answering tasks. 
 Use the following pieces of retrieved context to answer the latest question in the conversation. 
 If you don't know the answer, just say that you don't know. 
 The pre-existing conversation may provide important context to the question.
 Use three sentences maximum and keep the answer concise.
+Provide examples with code whenever possible, this really helps the user get started!
 
 Conversation: {conversation}
 Context: {context} 
@@ -17,6 +26,7 @@ Question: {question}
 Answer:"""
 
 def get_vector_db_retriever():
+    nest_asyncio.apply()
     persist_path = os.path.join(tempfile.gettempdir(), "union.parquet")
     embd = OpenAIEmbeddings()
 
@@ -46,3 +56,39 @@ def get_vector_db_retriever():
     )
     vectorstore.persist()
     return vectorstore.as_retriever(lambda_mult=0)
+
+retriever = get_vector_db_retriever()
+llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+# Define Graph state
+class GraphState(TypedDict):
+    question: str
+    messages: Annotated[List[AnyMessage], operator.add]
+    documents: List[Document]
+
+# Define Nodes
+def retrieve_documents(state: GraphState):
+    messages = state.get("messages", [])
+    question = state["question"]
+    documents = retriever.invoke(f"{get_buffer_string(messages)} {question}")
+    return {"documents": documents}
+
+def generate_response(state: GraphState):
+    question = state["question"]
+    messages = state["messages"]
+    documents = state["documents"]
+    formatted_docs = "\n\n".join(doc.page_content for doc in documents)
+    
+    rag_prompt_formatted = RAG_PROMPT.format(context=formatted_docs, conversation=messages, question=question)
+    generation = llm.invoke([HumanMessage(content=rag_prompt_formatted)])
+    return {"documents": documents, "messages": [HumanMessage(question), generation]}
+
+def get_rag_app():
+    graph_builder = StateGraph(GraphState)
+    graph_builder.add_node("retrieve_documents", retrieve_documents)
+    graph_builder.add_node("generate_response", generate_response)
+    graph_builder.add_edge(START, "retrieve_documents")
+    graph_builder.add_edge("retrieve_documents", "generate_response")
+    graph_builder.add_edge("generate_response", END)
+
+    return graph_builder.compile()
